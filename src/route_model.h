8983522656ccaaba407f6f03d81beb18ddc72d4a
Don <dchwojko@mac.com>
2019-05-20 19:07:50 -0500

    CODE: The RouteModel Class - Add a private vector of Node objects named m_Nodes. This will store all of the nodes from the Open Street Map data. - Add a public "getter" method SNodes. This method should return a reference to the vector of Nodes stored as m_Nodes.

--- src/route_model.h
+++ src/route_model.h
@@ -6,6 +6,11 @@
 #include "model.h"
 #include <iostream>
 
+/*
+Add a private vector of Node objects named m_Nodes. This will store all of the nodes from the Open Street Map data.
+Add a public "getter" method SNodes. This method should return a reference to the vector of Nodes stored as m_Nodes.
+*/
+
 class RouteModel : public Model {
 
   public:
@@ -20,13 +25,18 @@ class RouteModel : public Model {
         // Add private Node variables and methods here.
         int index;
         RouteModel * parent_model = nullptr;
+        
     };
     
     // Add public RouteModel variables and methods here.
     RouteModel(const std::vector<std::byte> &xml);  
     std::vector<Node> path; // This variable will eventually store the path that is found by the A* search.
 
+    // CODE: The RouteModel Class - public getter function that returns the nodes
+    auto & SNodes() { return m_Nodes; }
+
   private:
     // Add private RouteModel variables and methods here.
-
+    // CODE: The RouteModel Class - Vector of nodes
+    std::vector<Node> m_Nodes;
 };
--- src/route_planner.h
+++ src/route_planner.h
@@ -10,8 +10,26 @@ class RoutePlanner {
   public:
     RoutePlanner(RouteModel &model, float start_x, float start_y, float end_x, float end_y);
     // Add public variables or methods declarations here.
+    float GetDistance();
 
   private:
     // Add private variables or methods declarations here.
     RouteModel &m_Model;
+    RouteModel::Node * start_node;
+    RouteModel::Node * end_node;
+    float distance;
 };
+
+/*
+
+To complete this exercise:
+1. Add the following private variables to the `RoutePlanner` class in `route_planer.h`:
+  - A `RouteModel` reference `m_Model`. This will refer to the model that you will be performing A\* search on.
+  - `RouteModel::Node` pointers `start_node` and `end_node`. These will point to the nodes in the model which are closest to our starting and ending points.
+  - A float `distance`. This variable will hold the total distance for the route that A\* search finds from `start_node` to `end_node`.
+2. Add the following public method to the `RoutePlanner` class in `route_planner.h`:
+  - A `GetDistance()` method. This is a public getter method for the `distance` variable, and should just return `distance`. This method will later be used to print out the total distance from `main.cpp`.
+3. Use an initializer list in the `RoutePlanner` constructor in `route_planner.cpp` to initialize the `m_Model` variable with the passed `RoutePlanner` reference `model`.
+
+Note that this exercise has no tests associated with it, but there will be tests in future exercises using the `RoutePlanner` class.
+*/
